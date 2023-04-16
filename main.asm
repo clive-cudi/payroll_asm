6d9290709e49748929ce0338d4d7942a76172536
clive-cudi <clivemaina41@gmail.com>
2023-04-16 17:57:26 +0300

    fixed-choice-picking

--- main.asm
+++ main.asm
@@ -9,9 +9,14 @@ section .data
 	introOption3 db "3. Exit", 0x0a, 0x00
 	chooseResult db "You chose: %d", 0x0a, 0x00
 	intFormat db "%d", 0
+	option: times 4 db 0 ; 32-bit int 8*4
+	STRING_TERMINATOR equ 0
+	readEmployeeDetailsIntro db "Reading employee details", 0x0a, 0x00
+	updateEmployeeDetailsIntro db "update employee details", 0x0a, 0x00
 
 section .bss
-	introOptionChoiceBuffer resd 1
+	introOptionChoiceBuffer resb 25
+	introOptionChoiceBufferLen equ $- introOptionChoiceBuffer
 
 section .text
 	main:
@@ -22,45 +27,125 @@ section .text
 		; log statements
 		push introPrompt
 		call printf
+		add esp, 4
 
 		push introOption1
 		call printf
+		add esp, 4
 
 		push introOption2
 		call printf
+		add esp, 4
 
 		push introOption3
 		call printf
+		add esp, 4
 
-		; read input for introOptions
-		mov eax, 3
-		mov ebx, 0 ; stdout
-		mov ecx, introOptionChoiceBuffer,
-		mov edx, 1
-		int 0x80
+		; read input
+		push option
+		push intFormat
+		call scanf
+		add esp, 8
 
-		mov esi, [introOptionChoiceBuffer]
-		push esi
+		push dword [option]
 		push chooseResult
 		call printf
+		add esp, 8
 
+		mov eax, dword [option]
 
-		; end program epilogue
+		cmp eax, 1
+		je read_employee_details
+
+		cmp eax, 2
+		je update_employee_details
+
+		cmp eax, 3
+		je exit
+
+		; no valid option chosen
+		jmp exit
+
+read_employee_details:
+		push readEmployeeDetailsIntro
+		call printf
+		add esp, 4
+		jmp exit
+
+update_employee_details:
+		push updateEmployeeDetailsIntro
+		call printf
+		add esp, 4
+		jmp exit
+
+exit:		; end program epilogue
 		mov eax, 0 ; exit
 		mov esp, ebp
 		pop ebp
 		ret
 
-	log_to_console:
-		push ebp
-		mov ebp, esp
+	; Input:
+	; ESI = pointer to the string to convert
+	; ECX = number of digits in the string (must be > 0)
+	; Output:
+	; EAX = integer value
+	string_to_int:
+		xor ebx,ebx    ; clear ebx
+		.next_digit:
+			movzx eax,byte[esi]
+			inc esi
+			sub al,'0'    ; convert from ASCII to number
+			imul ebx,10
+			add ebx,eax   ; ebx = ebx*10 + eax
+			loop .next_digit  ; while (--ecx)
+			mov eax,ebx
+			ret
 
-		mov eax, [ebp+8]
 
-		push eax
-		call printf
+	; Input:
+	; EAX = integer value to convert
+	; ESI = pointer to buffer to store the string in (must have room for at least 10 bytes)
+	; Output:
+	; EAX = pointer to the first character of the generated string
+	int_to_string:
+		add esi,9
+		mov byte [esi],STRING_TERMINATOR
 
-		mov eax, 0
-		mov esp, ebp
-		pop ebp
-		ret
+		mov ebx,10         
+		.next_digit:
+			xor edx,edx         ; Clear edx prior to dividing edx:eax by ebx
+			div ebx             ; eax /= 10
+			add dl,'0'          ; Convert the remainder to ASCII 
+			dec esi             ; store characters in reverse order
+			mov [esi],dl
+			test eax,eax            
+			jnz .next_digit     ; Repeat until eax==0
+			mov eax,esi
+			ret
+
+	; read_employee_details:
+	; 	push ebp
+	; 	mov ebp, esp
+
+	; 	push readEmployeeDetailsIntro
+	; 	call printf
+	; 	add esp, 4
+		
+	; 	; clean up
+	; 	mov eax, 0
+	; 	mov esp, ebp
+	; 	pop ebp
+	; 	ret
+
+	; update_employee_details:
+	; 	push ebp
+	; 	mov ebp, esp
+
+	; 	push updateEmployeeDetailsIntro
+	; 	call printf
+	; 	add esp, 4
+
+	; 	mov eax, 0
+	; 	mov esp, ebp
+	; 	pop ebp
+	; 	ret
---
+++ sandbox/link_c_compile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo $0 usage is: $0 \<filename\>
+    echo "filename must end in a .asm"
+    exit 1
+fi
+#get the filename in a nicer named variable
+fileName=$1
+# echo $fileName
+
+#get last 4 characters of filename to make sure they match
+endChar=${fileName:$[${#fileName}-4]}
+ext=".asm"
+
+# echo $endChar
+
+#if ["$endChar" -ne "$ext"]; then
+#	echo "not equal"
+#fi
+#It takes the filename, and returns a substring 
+#starting from index 0, and pulls in the length 
+#of the filename -4 number of characters
+#to strip off the ".asm" at the end of the filename
+# echo "in loop"
+truncFileName=${fileName:0:$[${#fileName}-4]}
+nasm -f elf32 $fileName -o ./out/$truncFileName.o
+gcc -m32 ./out/$truncFileName.o -o ./out/$truncFileName
+
+cd ./out && ./$truncFileName
